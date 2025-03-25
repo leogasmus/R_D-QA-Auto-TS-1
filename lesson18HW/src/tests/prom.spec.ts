d91ca7aeecbd04c0ebf8fc865df238f2a7eb30a3
leogasmus <leogasmus@gmail.com>
2025-03-25 23:06:27 +0100

    [HW] Add homework for 18 lesson

---
+++ lesson18HW/src/tests/prom.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import { HomePage } from '../pom/home.page';
+import { Sidebar } from '../pom/sidebar.page';
+
+test.describe('Prom ', () => {
+    test('can search', async ({ page }) => {
+        const expectedProduct = 'iPhone';
+        const homePage = new HomePage(page);
+
+        await homePage.goto();
+        await expect(page).toHaveURL('https://prom.ua');
+        await homePage.searchProduct(expectedProduct);
+        const actualProducts = await homePage.getProductName();
+        expect(actualProducts.length).toBeGreaterThan(0);
+        for (const actualProduct of actualProducts) {
+            expect(actualProduct.toLowerCase()).toContain(expectedProduct.toLowerCase());
+        }
+    });
+
+    test('should promote application', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const sidebar = new Sidebar(page);
+        const expectedText = 'Агов! А в додатку зручніше';
+
+        await homePage.goto();
+        await expect(page).toHaveURL('https://prom.ua');
+        await homePage.openSideBar();
+        await sidebar.waitSideBarOpen();
+        await expect((await sidebar.getQrCode()).isVisible).toBeTruthy();
+        const actualText = (await sidebar.getAppPromoBanner()).textContent();
+        await expect(await actualText).toContain(expectedText);
+    });
+});

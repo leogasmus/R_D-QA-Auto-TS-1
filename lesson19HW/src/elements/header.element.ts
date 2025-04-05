ac04665f32a7fab5ccaa774f40c74cde5e7cadd9
leogasmus <leogasmus@gmail.com>
2025-04-05 21:21:03 +0200

    Add HW for lesson 19

---
+++ lesson19HW/src/elements/header.element.ts
@@ -0,0 +1,32 @@
+import { Locator } from '@playwright/test';
+
+export class HeaderElement {
+    private get searchInput(): Locator {
+        return this.baseLocator.locator('input[placeholder="Я шукаю..."]');
+    }
+
+    private get searchButton(): Locator {
+        return this.baseLocator.locator('[data-qaid="search_btn"]');
+    }
+
+    private get showSidebar(): Locator {
+        return this.baseLocator.locator('[data-qaid="show_sidebar"]');
+    }
+
+    private get caption(): Locator {
+        return this.baseLocator.locator('[data-qaid="caption"]');
+    }
+
+    public constructor(private baseLocator: Locator) {}
+
+    public async clickOnSidebar(): Promise<void> {
+        await this.showSidebar.waitFor();
+        await this.showSidebar.click();
+    }
+    public async fillSearchInputAndClick(product: string): Promise<void> {
+        await this.searchInput.waitFor();
+        await this.searchInput.fill(product);
+        await this.searchButton.click();
+    }
+
+}
